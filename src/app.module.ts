4c557e6c65552a399b774c6e66183e7629105295
aguaman123 <anderson.guaman@udla.edu.ec>
2025-04-04 17:46:32 -0500

    Actualizacion_Base

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,31 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ClienteModule } from './cliente/cliente.module';
 import { SwaggerModule } from '@nestjs/swagger';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT!,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+
+    SwaggerModule,
     ClienteModule, 
-    SwaggerModule],
-  controllers: [AppController],
-  providers: [AppService],
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
